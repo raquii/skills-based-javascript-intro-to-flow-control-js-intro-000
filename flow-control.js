df5b5e3dc753c582ea06cf7d2ff20636c69b579b
Raquel Román-Rodriguez <raquelanaroman@gmail.com>
2021-04-20 20:43:09 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -17,8 +17,7 @@ function teenager(age){
 function ageChecker(age){
     if (age > 12 && age < 20 ){
     return "You are a teenager!"
-  }
-  if else (age < 12 ){
+  } if else (age < 12 ){
     return "You are a kid"
       
     }
